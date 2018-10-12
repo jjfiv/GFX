743f6d5cacc57d1a3f204d43425e91cee34377f7
John Foley <jfoley@cs.umass.edu>
2018-10-12 10:08:55 -0400

    example for 1.5, dev version

---
+++ src/main/java/me/jjfoley/gfx/ex/ViewerExample.java
@@ -0,0 +1,44 @@
+package me.jjfoley.gfx.ex;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import me.jjfoley.gfx.GFX;
+import me.jjfoley.gfx.TextBox;
+
+public class ViewerExample extends GFX {
+	TextBox guess;
+	
+	public ViewerExample() {
+		this.guess = new TextBox("Your Latest Text");
+		this.guess.setColor(Color.white);
+		this.guess.centerInside(this.windowAsRectangle());
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		guess.draw(g);
+	}
+	
+	public void run() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			System.out.print("> ");
+			String text = reader.readLine();
+			if (text == null) {
+				break;
+			}
+			guess.setString(text);
+			guess.centerInside(this.windowAsRectangle());
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		ViewerExample ex = new ViewerExample();
+		ex.startViewer();
+		ex.run();
+	}
+}
